bb5b755023bb7727c5b4a292c431602cca7f2233
William Rojas <william.ur@live.com>
2021-02-15 19:00:22 -0700

    adjusted package.json to run proper test with jest, also updated css files to be scss files

--- src/client/js/testFormHandler.test.js
+++ src/client/js/testFormHandler.test.js
@@ -1,3 +1,8 @@
+// Import the js file to test
+import { handleSubmit } from './formHandler'
+
+
+
 // The describe() function takes two arguments - a string description, and a test suite as a callback function.  
 // A test suite may contain one or more related tests    
 describe("Testing the submit functionality", () => {
